09da1effa00a16f93dd1757471beb983cf56caec
Tiphainejh <Tiphainejh@users.noreply.github.com>
2025-01-19 21:27:10 +0000

    deploy: 048e31db4cb260b1e156aedfd723d729ba83c56d

--- docs/classsrc_1_1data__processing_1_1DataProcessor.js
+++ docs/classsrc_1_1data__processing_1_1DataProcessor.js
@@ -9,6 +9,7 @@ var classsrc_1_1data__processing_1_1DataProcessor =
     [ "extract_language", "classsrc_1_1data__processing_1_1DataProcessor.html#a8c28d40e8b07b8de8a765f8f5c9c18f8", null ],
     [ "extract_lemma", "classsrc_1_1data__processing_1_1DataProcessor.html#a3a6f7d8ef4426dfe180524a089fe5ea9", null ],
     [ "extract_word", "classsrc_1_1data__processing_1_1DataProcessor.html#aca8cb2dd99e8358d0c80ff0f04307193", null ],
+    [ "process_data", "classsrc_1_1data__processing_1_1DataProcessor.html#af1d408b78a8a28b54b96e30bc3de1754", null ],
     [ "process_hanja_data", "classsrc_1_1data__processing_1_1DataProcessor.html#a611dcfaf4a6c3129fc38bc01d659203f", null ],
     [ "read_hanja_file", "classsrc_1_1data__processing_1_1DataProcessor.html#acee13daf812b03ebd4b468b5bd21d832", null ],
     [ "folder_path", "classsrc_1_1data__processing_1_1DataProcessor.html#a3f151d5d1bb0d7794aacf774691d3d1f", null ]
