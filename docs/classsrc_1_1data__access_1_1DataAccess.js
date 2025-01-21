96339b59770eb380c587b8198e415f278a3d1f67
Tiphainejh <Tiphainejh@users.noreply.github.com>
2025-01-21 17:15:42 +0000

    deploy: 5ca6d898f607078287fb59668e953a236ec74687

--- docs/classsrc_1_1data__access_1_1DataAccess.js
+++ docs/classsrc_1_1data__access_1_1DataAccess.js
@@ -3,6 +3,7 @@ var classsrc_1_1data__access_1_1DataAccess =
     [ "drop_tables", "classsrc_1_1data__access_1_1DataAccess.html#a23e8da2b908674f31c01407620fe3728", null ],
     [ "get_hanja_for_word", "classsrc_1_1data__access_1_1DataAccess.html#a6c59fef88695c46c9ee18f370e4346c6", null ],
     [ "get_hanja_meanings_for_word", "classsrc_1_1data__access_1_1DataAccess.html#ae726f5ea5aa704b34208db1cc915fa3a", null ],
+    [ "get_related_words", "classsrc_1_1data__access_1_1DataAccess.html#a1f8eb51816b5631748421df5638689e4", null ],
     [ "get_word_by_korean", "classsrc_1_1data__access_1_1DataAccess.html#a3f2fa8212ead4b1885e8f26697b4b0b9", null ],
     [ "initialize_database", "classsrc_1_1data__access_1_1DataAccess.html#a4684f7cefabfbcb84b28be9e2ef74f64", null ],
     [ "insert_data", "classsrc_1_1data__access_1_1DataAccess.html#a4f4e7ab71ee7131858752bdfb4d0e2f1", null ],
